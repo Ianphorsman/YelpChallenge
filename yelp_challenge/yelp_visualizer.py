780c761e032d805c0c3ee8f2c13b6950b8c023ab
Ian Horsman <ianphorsman@gmail.com>
2018-03-23 22:41:24 -0400

    added function to browser photos in yelp photos dataset

--- yelp_challenge/yelp_visualizer.py
+++ yelp_challenge/yelp_visualizer.py
@@ -1,11 +1,21 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from yelp_challenge.yelp_data_loader import DataLoader
 
 class Visualizer(object):
 
     def __init__(
         self
     ):
-        pass
+        self.data_loader = DataLoader()
 
+    def browse_photos(self):
+        photos = self.data_loader.iter_photos()
+        for i, photo in enumerate(photos):
+            plt.figure()
+            plt.imshow(photo)
+            plt.show()
+
+v = Visualizer()
+v.browse_photos()
