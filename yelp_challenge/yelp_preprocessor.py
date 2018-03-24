3ce4fd5e9636cd47cf388965b430d0eb4d70aa59
Ian Horsman <ianphorsman@gmail.com>
2018-03-23 20:26:12 -0400

    Path resolver

--- yelp_challenge/yelp_preprocessor.py
+++ yelp_challenge/yelp_preprocessor.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 import numpy as np
 import pandas as pd
 import tensorflow as tf
@@ -8,4 +9,16 @@ class Preprocessor(object):
     def __init__(
         self
     ):
-        pass
+        self.paths = self.resolve_data_paths()
+        assert len(self.paths.keys()) >= 1, "Please supply paths to your Yelp json and/or image datasets in the data_env.json file"
+
+    
+
+    def resolve_data_paths(self):
+        with open('../data_env.json') as file:
+            data_env = json.load(file)
+        return {k: v for k, v in data_env.items() if Path(v).exists() and v != ""}
+
+
+
+preprocessor = Preprocessor()
